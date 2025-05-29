26eedddd4a862a38382158937043a955068cffd0
Marios Tsou <tsoumarios@gmail.com>
2025-05-29 14:49:52 +0300

    Logo

--- frontend/my-react-frontend/src/components/navBar/NavBarLeft.jsx
+++ frontend/my-react-frontend/src/components/navBar/NavBarLeft.jsx
@@ -1,5 +1,7 @@
+import logo from '../../assets/Logo-lube.png'; 
 const NavbarLeft = () => (
   <div className="navbar-left">
+    <img src={logo} alt="LubeCRM Logo" className="logo" width={50} height={50} />
     <h2>LubeCRM</h2>
   </div>
 );
