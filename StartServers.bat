da0c9904e087c86574cb6301ad4b6bd57c7d17f0
Marios <tsoumarios@gmail.com>
2025-07-15 18:02:40 +0300

    Start MongoDB in Bat

--- StartServers.bat
+++ StartServers.bat
@@ -1,7 +1,9 @@
 @echo off
+REM Start MongoDB service
+net start MongoDB
+
 REM Start backend server
 start "Backend Server" /min cmd /k "cd backend && node server.js"
 
 REM Start frontend server
 start "Frontend Server" /min cmd /k "cd frontend\my-react-frontend && npm run dev"
-
